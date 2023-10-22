edc0c0976424c2658e280b6cce5b57023dd8be60
Penumatcha Bharath Varma <50948937+Bharath-varma-p@users.noreply.github.com>
2023-10-22 14:02:58 -0400

    Get Post put delete in Exp

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ var express = require('express')
 var app = express();
 
 
+app.get('/',function(req,res) {
+    //res.send('<html><body><h1>Hello Interns How are you </h1></body></html>')
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+app.get('/contact',function(req,res){
+    res.send('<html><body><h1>Hai I am in Contact Page</h1></body></html>')
+})
+
+
+
+
+//app object includes 4 function get() post() put() delete()... 
 
 
 var server = app.listen(3000, function(){
